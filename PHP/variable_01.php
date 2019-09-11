3b2aa3a2504c4a58ba4c83dd25197a3cf0f3b42f
seokjookang <kangsukjoo1@naver.com>
2019-09-11 14:08:43 +0900

    variable test 	#	new file:   script/variable_01.js#

---
+++ PHP/variable_01.php
@@ -0,0 +1,30 @@
+<?php
+$foo = "대림대학교";
+echo $foo;
+
+$age = 17;
+$AGE = 20.5;
+$name = "대림대학교";
+$asci = 'hello';
+
+
+echo $age."\n";
+echo $AGE."\n";
+echo $name."\n";
+echo $name[0]."\n";
+
+$foo2 = "대림이";
+$bar = $foo2;
+$foo2 = "대숙이";
+echo $foo2."\n";
+echo $bar."\t";
+
+//int age;
+var_dump($age);
+
+//is_int($age)
+
+$temp = 20.5;
+$name = "\n"."현제 온도는 $temp 입니다.";
+
+echo $name;
---
+++ script/variable_01.js
@@ -0,0 +1,27 @@
+foo = "대림대학교";
+console.log(foo);
+
+age = 17;
+AGE = 20.5;
+
+name = "대림대학교";
+asci = 'hello';
+
+console.log(age)+"\n";
+console.log(AGE)+"\n";
+console.log(name+"\n");
+console.log(asci[0]+"\n");
+
+var foo2 = "대림이";
+var bar = foo2;
+ foo2 = "대숙이";
+
+console.log(foo2);
+console.log(bar);
+
+console.log(typeof age);
+
+let temp = 20.5;
+const name2 = "현재 온도는 $(temp) 입니다.";
+
+console.log(name2);
