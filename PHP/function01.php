47f32bb6fa31e49abc7ac601722f256e4573e853
seokjookang <kangsukjoo1@naver.com>
2019-09-18 13:49:49 +0900

    test

---
+++ PHP/function01.php
@@ -0,0 +1,18 @@
+<?php
+
+$naem = "대림이";
+
+//선언
+if(!function_exists()) {
+    function hello()
+    {
+        echo "반가워요".$name;
+    }
+} // 함수 펑션 헬로가 없으면 헬로 만들어줘용
+
+
+//선언은 2번 선언 안됌
+
+//호출
+hello();
+hello();
