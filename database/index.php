59545217f6a9bca6150b17b284bab851f6515d50
seokjookang <kangsukjoo1@naver.com>
2019-10-30 11:10:39 +0900

    업로드 실패돼있어서 다시

--- database/index.php
+++ database/index.php
@@ -1,8 +1,5 @@
 <?php
 
-// 윈도우 os : 파일명 대소문자 구분 없음
-// 리눅스 os : 파일명 대소문자 구별,
-
 $config = include "dbconf.php";
 
 require "database.php";
@@ -10,59 +7,15 @@ require "table.php";
  // 키값이 있는 연상배열
 
 $db = new Database($config);
-// 3 방금 메소드 체인 한줄로 바꿈 , 4 Table($db)를 불러옴
-/// $db->setTable(new Table($db))->getTable()->createTable(); // 2테이블 객체를 생성해서 연결
- // 1연결된 객체를 읽어옴
-// database 클래스에 Table 프로퍼티 갖구와서 거기에 Table 클래스 객체생성
 
-$db->getTable()->createTable(
-    "members2", // << $name
-    [
-        'Last'=>"varchar(50)",
-        'First'=>"varchar(50)",
-        'Age'=>"varchar(50)",
-    ]);
+$query = "SHOW TABLES";
+$result = $db->queryExecute($query); //db에서 생성한 Database에서 queryExecute를 호출
 
-if($db->isTable("members")){
-    echo "테이블존재함";
-}else{
-    echo "테이블 없음";
-}
+$count = mysqli_num_rows($result); //테이블 갯수나오는 함수
+echo "테이블 갯수는 = ".$count;
+echo "</br>";
 
-// // phpinfo();
-
-// // 서버정보
-// // $host = "localhost";
-// // $user = "phpuser";
-// // $passwd = "multi1004";
-// // $database = "php";
-// include "dbconf.php";
-
-// // mysql 접속
-// $connect = new mysqli($host, $user, $passwd, $database);
-// // 성공 : connect_errno = 0
-// // 실패 : connect_errno = 1
-
-// if(!$connect->connect_errno){
-//     echo "DB 접속 성공이에요";
-// }else{
-//     echo "접속이 안돼요 ㅜ";
-// }
-
-// $query = "
-// CREATE TABLE `members` (
-//     `id` int(11) NOT NULL AUTO_INCREMENT,
-//     `LastName` varchar(255),
-//     `FirstName` varchar(255),
-//     PRIMARY KEY(`id`)
-//     ) ENGINE=innodb default charset=utf8;
-//     ";
-
-//     // mysqli 쿼리 에 콘넥트 변수에 쿼리를 씌우겠다??
-//     if(mysqli_query($connect,$query)){
-//         echo "테이블 생성 성공";
-//     }else{
-//         print "테이블 생성 실패";
-//     }
-//     빽틱이랑 작은따움표를 구분을한다
-// errno 는 프롬퍼티
+for($i=0; $i<$count; $i++){
+    $row = mysqli_fetch_object($result);  // 
+    echo $row -> Tables_in_php."<br>";
+    }
--- database/index2.php
+++ database/index2.php
@@ -2,5 +2,8 @@
 
 include "test.php";
 
-$obj = new Test();
+$obj = Test::make();
+$obj->factory("Table"); // new Table 과 같음 근데 이름 수정하면 바꿔야돼는데 이건 괄호안만 바꾸면됌
 
+// 게임 으로 비유하면 칼모드 , 총모드 같이 할수있다 실시간으로 변경가능 니달리 생각하면 편함
+// $obj->strage(object $algorism);
